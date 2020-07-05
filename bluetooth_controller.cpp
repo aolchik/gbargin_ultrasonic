3499d87d41086e194f68c9e44ef1c9399cedd998
Alejandro Olchik <alejandro@orulo.com.br>
2020-07-05 14:25:37 -0300

    Enable to control motor speed.

--- bluetooth_controller.cpp
+++ bluetooth_controller.cpp
@@ -5,64 +5,70 @@ BluetoothController::BluetoothController(Logger* l, Engine* e) {
   this->engine = e;
   this->bluetoothSerial = new SoftwareSerial(12,13);
   this->bluetoothSerial->begin(9600); 
+  this->autoModeEnabled = 0;
+  this->iSpeed = 0;
 }
 
 BluetoothController::~BluetoothController() {
   delete this->bluetoothSerial;
 }
 
-void BluetoothController::followCommand() {
-  if (this->bluetoothSerial->available()) { //check if bluetooth command available
-    this->val = this->bluetoothSerial->read();
-    //this->bluetoothSerial->write(this->val);
-    this->logger->debug(this->val);
-  }
-  
-  if (this->val == 'F') { // Forward
-    this->engine->forward();
-  }
-  else if (this->val == 'S') { // Stop Forward
-    this->engine->stop();
-  }
-  else if (this->val == 'B') { // Backwards
-    this->engine->backward();
-  }
-  else if (this->val == 'R') { // Right
-    this->engine->right();
-  }
-  else if (this->val == 'L') { // Left
-    this->engine->left();
-  }
-  else if (this->val == 'G') { // Forward Left
-    this->engine->forwardLeft();
-  }
-  else if (this->val == 'I') { // Forward Right
-    this->engine->forwardRight();
-  }
-  else if (this->val == 'H') { // Backward Left
-    this->engine->backwardLeft();
-  }
-  else if (this->val == 'J') { // Backward Right
-    this->engine->backwardRight();
-  }
-
-//  else if (this->val == 's') { // Stop, not used though
-//    stopp(10 ) ;
-//  }
-//  else if (int(this->val) >= 49 && int(this->val) <= 57) { //set speed
-//    iSpeed = (int(this->val)-48)*26;
-//    Serial.println("Speed set to: " + iSpeed); 
-//  }
-//  else if (this->val == 'q') { //set speed
-//    iSpeed = 255;
-//    digitalWrite(LedPin,HIGH);   
-//    Serial.println("Speed set to: " + iSpeed);  
-//  }
-//  else if (this->val == 'W') {
-//    digitalWrite(LedPin,HIGH);
-//  }
-//  else if (this->val == 'w') {
-//    digitalWrite(LedPin,LOW);
-//  }
+int BluetoothController::getAutoMode() {
+  return this->autoModeEnabled;
 }
 
+int BluetoothController::getSpeed() {
+  return this->iSpeed;
+}
+
+void BluetoothController::followCommand() {
+  if (this->bluetoothSerial->available()) {
+    this->val = this->bluetoothSerial->read();
+    //this->bluetoothSerial->write(this->val);
+    this->logger->debug(this->val); // @FIXME: Seems this log is not working
+
+    if (this->val == 'S') { // Stop Forward
+        this->engine->stop();
+    }
+    else if (this->val == 'F') { // Forward
+      this->engine->forward(this->iSpeed);
+    }
+    else if (this->val == 'B') { // Backwards
+      this->engine->backward(this->iSpeed);
+    }
+    else if (this->val == 'R') { // Right
+      this->engine->right(this->iSpeed);
+    }
+    else if (this->val == 'L') { // Left
+      this->engine->left(this->iSpeed);
+    }
+    else if (this->val == 'G') { // Forward Left
+      this->engine->forwardLeft(this->iSpeed);
+    }
+    else if (this->val == 'I') { // Forward Right
+      this->engine->forwardRight(this->iSpeed);
+    }
+    else if (this->val == 'H') { // Backward Left
+      this->engine->backwardLeft(this->iSpeed);
+    }
+    else if (this->val == 'J') { // Backward Right
+      this->engine->backwardRight(this->iSpeed);
+    }
+    else if (int(this->val) >= 49 && int(this->val) <= 57) { //set speed
+      this->iSpeed = (int(this->val)-48)*26;
+      Serial.println("Speed set to: " + this->iSpeed);
+    }
+    else if (this->val == 'q') { //set speed
+      this->iSpeed = 255;
+      Serial.println("Speed set to: " + this->iSpeed);
+    }
+    else if (this->val == 'U') { //set speed
+      this->autoModeEnabled = 1;
+      Serial.println("Automode enabled!");
+    }
+    else if (this->val == 'u') { //set speed
+      this->autoModeEnabled = 0;
+      Serial.println("Automode disabled!");
+    }
+  }
+}
